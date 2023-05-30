966bc706a42873b4073762ad03290126552fb375
Petras <petrosenas@gmail.com>
2023-05-30 19:17:30 +0300

    Numbers

--- js/02-numbers.js
+++ js/02-numbers.js
@@ -1,1 +1,47 @@
 console.clear();
+
+/*
+NUMBERS
+Sveikieji/desimtainiai
+Teigiami/neigiami
+Mistiniai: Begalybes ir NAN (not-a-number)
+
+Aritmetiniai operatoriai:
++,-,*,/,
+Salia matematikos:
+(, )
+
+Kintamuju inicijavimo budai:
+const - default
+let - naudoti, tik jei reikia keisti reiksmes
+var - senove, todel niekada nenaudoti nebent TIKRAI zinai ka darai
+
+*/
+
+console.log(1);
+console.log(3.14);
+console.log(NaN);
+console.log(2 + 3);
+console.log(0.1+0.2, 0.3);
+
+// console.log(1);
+
+// KINTAMIEJI
+
+const amzius = 20;
+console.log(amzius);
+
+console.clear();
+// Kintamieji: konstantos ir kintamieji
+
+let pinigine = 0
+console.log(pinigine);
+
+pinigine = 5
+console.log(pinigine);
+
+pinigine = 3+4
+console.log(pinigine);
+
+pinigine = pinigine + 5
+console.log(pinigine);
