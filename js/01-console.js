b6597aa4da5a9a4e4a84004b70460d225cc53c6c
Petras <petrosenas@gmail.com>
2023-05-30 18:23:19 +0300

    Comments

--- js/01-console.js
+++ js/01-console.js
@@ -3,3 +3,12 @@ console.clear();
 console.log(1);
 console.log(7);
 console.log(1,4,5);
+
+// Single-line comment
+
+/*Single-line as multi-line comment*/
+
+/*Single-line 
+as 
+multi-line 
+comment*/
