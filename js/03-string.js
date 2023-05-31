061077aad75059e73de214a7f019bdb5142ba7b5
Petras <petrosenas@gmail.com>
2023-05-31 20:34:14 +0300

    String

--- js/03-string.js
+++ js/03-string.js
@@ -7,4 +7,13 @@ Inicijavimo budai:
 - viengubos kabutes ('')
 - dvigubos kabutes ("")
 */
+
 const vardas = 'Jonas';
+const vardas2 = "Maryte";
+console.log(vardas);
+console.log(vardas2);
+
+const firstName = 'Petras';
+const lastName = 'Vedeckas';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
